03630b6fdef90d728621ec73ff237856062f6c46
Oskar Wyke <oskar.wyke@gmail.com>
2016-02-22 23:29:20 +0100

    Fixing mess

--- RxJava Spring Boot/src/main/java/se/wyko/rxjava/springboot/RestApi.java
+++ RxJava Spring Boot/src/main/java/se/wyko/rxjava/springboot/RestApi.java
@@ -4,17 +4,13 @@ package se.wyko.rxjava.springboot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
 
 
 @Controller
