77a25ffcbaa18db678cbabea7a88d3d6bc503ea2
Oskar Wyke <oskar.wyke@gmail.com>
2016-02-22 20:35:48 +0100

    Cleanup

--- RxJava Spring Boot/src/main/java/se/wyko/rxjava/springboot/RestApi.java
+++ RxJava Spring Boot/src/main/java/se/wyko/rxjava/springboot/RestApi.java
@@ -1,19 +1,16 @@
 package se.wyko.rxjava.springboot;
 
 
-import org.glassfish.jersey.client.rx.Rx;
-import org.glassfish.jersey.client.rx.rxjava.RxObservableInvoker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import rx.Observable;
-
-import javax.ws.rs.client.ClientBuilder;
 
 
 @Controller
